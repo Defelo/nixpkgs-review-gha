1dedd9cac96be29a90b1886071172d93b15b78b5
Defelo <mail@defelo.de>
2025-08-09 22:38:21 +0200

    feat: add section for still failing packages to report

--- flake.nix
+++ flake.nix
@@ -27,6 +27,14 @@
     in
 
     {
+      packages = eachSystem (pkgs: {
+        generate-markdown-report = pkgs.writers.writePython3Bin "generate-markdown-report" {
+          flakeIgnore = [
+            "E501" # line too long
+          ];
+        } (builtins.readFile ./generate_markdown_report.py);
+      });
+
       overlays.nixpkgs-review = final: prev: {
         inherit (nixpkgs-review.packages.${final.system}) nixpkgs-review;
       };
@@ -45,6 +53,7 @@
 
       checks = eachSystem (pkgs: {
         inherit (pkgs) nixpkgs-review;
+        packages = pkgs.linkFarm "packages" self.packages.${pkgs.system};
         fmt = pkgs.runCommandNoCCLocal "fmt-check" { } ''
           cp -r --no-preserve=mode ${./.} repo
           ${lib.getExe self.formatter.${pkgs.system}} -C repo --ci --tree-root-file=treefmt.nix
